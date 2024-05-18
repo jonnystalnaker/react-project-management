79a39c099d1a4642c99d9c6b976b8f422dbe7772
jonnystalnaker <steamsynthetic@gmail.com>
2024-05-18 11:25:14 -0700

    adding state for new projects and hoisting data

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,25 @@ function App() {
     });
   }
 
+  function handleAddProject(projectData) {
+    setProjetsState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random().toString(),
+      };
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  console.log(projectsState);
+
   let content;
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject />;
+    content = <NewProject onAdd={handleAddProject} />;
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
