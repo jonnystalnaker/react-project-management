c1da521f01b04075495e22ebc7fcfde9f1e4ff30
jonnystalnaker <steamsynthetic@gmail.com>
2024-05-21 13:56:20 -0700

    adding forward ref to modal

--- src/App.jsx
+++ src/App.jsx
@@ -20,18 +20,18 @@ function App() {
 
   function handleAddProject(projectData) {
     setProjetsState(prevState => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random().toString(),
+        id: projectId,
       };
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
   }
-
-  console.log(projectsState);
 
   let content;
 
@@ -43,7 +43,10 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
